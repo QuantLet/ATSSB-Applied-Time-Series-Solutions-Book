b76d6af9ca614482583ae9cbf92bd5254dadf34f
Rainy1993 <474858566@qq.com>
2025-06-16 22:04:54 +0800

    Add files via upload

---
+++ ATSSB_Stationary_Time_Series_Models/ATSSB_ARMA.py
@@ -0,0 +1,74 @@
+
+# In terminal
+# pip install pythontsa
+# pip install --upgrade matplotlib
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.arima_process import arma_generate_sample
+
+from PythonTsa.plot_acf_pacf import acf_pacf_fig
+from PythonTsa.True_acf import Tacf_pacf_fig
+
+import statsmodels.api as sm
+
+
+path = "/Users/ruting/Documents/Github/pyTSA/ATSSB_Stationary_Time_Series_Models"
+os.chdir(path)
+
+arp=[-0.4, 0.8, -0.8, 1]
+
+arroot=np.roots(arp)
+abs(arroot)
+
+map=[0.3, -0.6, 0.2, 1]
+maroot=np.roots(map)
+abs(maroot)
+arp3=[-0.6, 0.8, -0.7, 1]
+arroot3=np.roots(arp3)
+
+abs(arroot3)
+
+map3=[0.3, -0.6, -0.2, 1]
+
+maroot3=np.roots(map3)
+
+abs(maroot3)
+
+ar1=[1]
+ma1=[1, 0.2, -0.6, -0.3]
+
+Tacf_pacf_fig(ar1,ma1, both=True, lag=20)
+plt.savefig('TrueACFProb311-1.eps', dpi = 1200, 
+            bbox_inches ='tight', transparent = True)
+plt.savefig('TrueACFProb311-1.png', dpi = 1200, 
+            bbox_inches ='tight', transparent = True)
+
+plt.show()
+
+ar2=[1, -0.7, 0.8, -0.6]
+ma2=[1]
+Tacf_pacf_fig(ar2,ma2, both=True, lag=20)
+plt.savefig('TrueACFProb311-2.eps', dpi = 1200, 
+            bbox_inches ='tight', transparent = True)
+plt.savefig('TrueACFProb311-2.png', dpi = 1200, 
+            bbox_inches ='tight', transparent = True)
+
+plt.show()
+
+ar3=[1, -0.7, 0.8, -0.6]
+ma3=[1, -0.2, -0.6, 0.3]
+
+Tacf_pacf_fig(ar3,ma3, both=True, lag=20)
+plt.savefig('TrueACFProb311-3.eps', dpi = 1200, 
+            bbox_inches ='tight', transparent = True)
+plt.savefig('TrueACFProb311-3.png', dpi = 1200, 
+            bbox_inches ='tight', transparent = True)
+
+plt.show()
+
+
+
+
+
